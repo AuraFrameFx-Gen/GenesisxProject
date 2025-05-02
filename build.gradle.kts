c738e0fdea3b81fe889a5adbd35cdaf06b8abe09
https://github.com/AuraFrameFX-Dev <wehttam1989@gmail.com>
2025-05-02 09:02:57 -0700

    Refactor: Add KSP plugin to root build script
    - Adds the KSP Gradle plugin to the root `build.gradle.kts` to enable Kotlin Symbol Processing.
    - Configures KSP plugin with classpath dependency in the root buildscript.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,16 @@ plugins {
     alias(libs.plugins.navigation.safe.args) apply false
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.16")
+    }
+}
+
 tasks.register("clean", Delete::class) {
     delete(layout.buildDirectory)
 }
