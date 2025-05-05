0ef622ae5139182daddbbe6fb2d8cbfcd8bbe79d
AuraFrameFx-Gen <wehttam1989@gmail.com>
2025-05-04 21:07:51 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,46 +3,25 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
-
-    // Configure plugin resolution strategy
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.namespace) {
-                "com.android" -> useModule("com.android.tools.build:gradle:${requested.version}")
-                "org.jetbrains.kotlin" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-                "com.google.dagger.hilt.android" -> useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
-            }
-        }
+    plugins {
+        id("com.android.application") version "8.8.2"
+        id("com.android.library") version "8.8.2"
+        id("org.jetbrains.kotlin.android") version "1.9.22"
+        id("com.google.dagger.hilt.android") version "2.50"
+        id("androidx.navigation.safeargs.kotlin") version "2.7.6"
     }
 }
 
-// Enable type-safe project accessors
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-// Enable version catalogs
-enableFeaturePreview("VERSION_CATALOGS")
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-
-    // Configure repositories for all projects
     repositories {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://api.xposed.info/") }
     }
 }
 
-// Version catalog is now defined in gradle/libs.versions.toml
-// and automatically loaded by Gradle
-
 rootProject.name = "AuraFrameFX"
-
-// Core application module
 include(":app")
-// Dynamic feature modules
-include(":visualization")
-// Library modules
-include(":XposedBridge-art")
