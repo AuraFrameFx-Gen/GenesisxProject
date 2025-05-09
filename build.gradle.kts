61c1121d98dc565fcf3717db2c1bd15968fea0aa
https://github.com/AuraFrameFX-Dev <wehttam1989@gmail.com>
2025-05-09 03:06:58 -0600

    Chore: Configure google services plugin
    - Adds the google services plugin dependency to the build script in the root `build.gradle.kts`.
    - Configures the Kotlin compiler options to use JVM target 17 and include specific free compiler arguments (`-Xjvm-default=all`, `-opt-in=kotlin.RequiresOptIn`).

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,28 @@ plugins {
     alias(libs.plugins.agp.app) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
-        alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.hilt.android) apply false
 
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2")
+    }
 }
 
 allprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = "17"
-            freeCompilerArgs += listOf(
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            freeCompilerArgs.addAll(listOf(
                 "-Xjvm-default=all",
                 "-opt-in=kotlin.RequiresOptIn"
-            )
+            ))
         }
     }
 
